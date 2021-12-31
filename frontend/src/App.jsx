24e886b090aa9823f7a468e4a285604b4b2b6291
AnthonyMerive <anthonycolmenaresrivas@gmail.com>
2021-12-31 01:39:51 -0400

    realizando peticion para luego crear el redux

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,23 @@
 import { Grid } from '@mui/material'
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect } from 'react'
 import { Form } from './components/Form'
 import { List } from './components/List'
 
 export default function App() {
+
+    useEffect(() => {
+        axios.get('https://citas-reactive.herokuapp.com/citasReactivas')
+            .then(function (response) {
+                // handle success
+                console.log(response.data);
+            })
+            .catch(function (error) {
+                // handle error
+                console.log(error);
+            })
+    }, [])
+
     return (
         <Grid container spacing={2}>
             <Grid item xs={12} sm={6}>
--- frontend/src/components/List.jsx
+++ frontend/src/components/List.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { DataGrid } from '@mui/x-data-grid';
+import axios from 'axios';
 
 export const List = () => {
+
+    const [rows, setRows] = useState([])
+    useEffect(() => {
+        axios.get('https://citas-reactive.herokuapp.com/citasReactivas')
+            .then(function (response) {
+                console.log(response.data);
+                setRows(response.data)
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+    }, [])
 
     const columns = [
         { field: 'id', headerName: 'ID', width: 70 },
@@ -13,17 +26,17 @@ export const List = () => {
         { field: 'horaReservaCita', headerName: 'Hora', width: 130 },
     ];
 
-    const rows = [
-        { id: 1, nombreMedico: 'Snow', apellidosMedico: 'Jon', age: 35 },
-        { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-        { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-        { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-        { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-        { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-        { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-        { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-        { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-    ];
+    // rows = [
+    //     { id: 1, nombreMedico: 'Snow', apellidosMedico: 'Jon', age: 35 },
+    //     { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
+    //     { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
+    //     { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
+    //     { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+    //     { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+    //     { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+    //     { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+    //     { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+    // ];
 
     return (
         <div style={{ height: 400, width: '100%' }} >
